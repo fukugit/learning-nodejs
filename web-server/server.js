6d213ab7730989b354d136b520fad2fcb9e35d97
fukugit <fukuma1234@gmail.com>
2020-03-31 18:11:40 +0900

    Add server js to learn http module.

---
+++ web-server/server.js
@@ -0,0 +1,14 @@
+// httpとはNode.jsが用意しているモジュールです。
+var http = require('http');
+var settings = require('./settings');
+var server = http.createServer();
+// Requestされた時の処理はコールバック関数に定義します。
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.write('hello world!!!');
+    res.end();
+});
+// サーバを起動します。 http://localhost:1337/ 起動後は右記へアクセスすることが可能です。
+// node server.js で起動できます。
+server.listen(settings.port, settings.host);
+console.log("server listening...");
